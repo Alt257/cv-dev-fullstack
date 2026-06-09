62ffc66420e78278216d5f88d172b86298f22013
benoît <bra@synapsia.fr>
2026-06-09 14:37:37 -0400

    Migrate CV to a dynamic, template-driven architecture using Twig. Restructure files, replace static HTML with templates, and update data source to YAML.

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Symfony\Component\Yaml\Yaml;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$yamlFilePath = dirname(__DIR__) . '/data/cv.yaml';
+$data = Yaml::parseFile($yamlFilePath);
+
+$loader = new FilesystemLoader(dirname(__DIR__) . '/templates');
+$twig = new Environment($loader, [
+    'cache' => false,
+    'debug' => true,
+]);
+
+echo $twig->render('cv.html.twig', $data);
